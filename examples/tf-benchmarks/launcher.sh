a11264e51260df37b7ea74963ed6a9204d6f0ce2
Jeremy Lewi <jeremy@lewi.us>
2017-12-02 15:21:45 -0800

    Delete code used for troubleshooting issues with the output
    Go back to using the official repo for the TF cnn benchmark code.

--- examples/tf-benchmarks/launcher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-echo Running launcher.sh
-python launcher.py $@
-chmod a+x /opt/run_benchmarks.sh
-/opt/run_benchmarks.sh
--- examples/tf-benchmarks/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-
-# Create the launch script
-python create_launch_command.py $@ > /opt/run_benchmarks.sh
-chmod a+x /opt/run_benchmarks.sh
-/opt/run_benchmarks.sh
